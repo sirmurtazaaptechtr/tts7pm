0d334e4c1ba611c80a66bb8d35af4362a371d98e
Syed Murtaza Hussain <sirmurtazaaptechtr@outlook.com>
2025-01-04 20:44:59 +0500

    Create and Read

--- newApp/form.php
+++ newApp/form.php
@@ -1,21 +1,43 @@
 <?php
     require('connection.inc.php');
     
+    $fullNameErr = $genderErr = '';
     $fullName = $email = $dob = $phone = $gender = '';
+    $message = '';
 
     if(isset($_POST['saveBtn']) && $_SERVER['REQUEST_METHOD'] == 'POST') {
         pr($_POST);
-        $fullName = test_input($_POST['fullName']);
+
+        if(empty($_POST['fullName'])) {
+            $fullNameErr = "Full Name is required!";
+        }else {
+            $fullName = test_input($_POST['fullName']);
+        }
+        
         $email = test_input($_POST['email']);
         $dob = test_input($_POST['dob']);
         $phone = test_input($_POST['phone']);
-        $gender = test_input($_POST['gender']);
 
+        if(empty($_POST['gender'])) {
+            $genderErr = "Gender is required!";
+        }else {            
+            $gender = test_input($_POST['gender']);
+        }
+
+        if(empty($fullNameErr) && empty($genderErr)) {
+            $insert_student_sql = "INSERT INTO `students` (name,email,dob,phone,gender) VALUES ('$fullName','$email','$dob','$phone','$gender')";
+            if($is_inserted = mysqli_query($conn,$insert_student_sql)) {
+                $message = "Student Inserted Successfully!";
+                $fullName = $email = $dob = $phone = $gender = '';
+                header("Location:students.php?message=" . urlencode($message));
+                exit();                
+            }
+        }
 
     }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 
 <head>
     <meta charset="UTF-8">
@@ -26,14 +48,18 @@
 
 <body>
     <div class="container">
-        <h1>Student Registration Form</h1>
+        <h1>Student Registration Form</h1>        
+        <p class="text-danger">* Required</p>
         <div class="row">
             <div class="col-6">
                 <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
                     <div class="mb-3 row">
                         <label for="fullName" class="col-sm-2 col-form-label">Full Name</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="fullName" name="fullName" placeholder="John Doe" value="<?php echo $fullName;?>">
+                        <div class="col-sm-8">
+                            <input type="text" class="form-control" id="fullName" name="fullName" placeholder="John Doe" value="<?php echo $fullName;?>">                            
+                        </div>
+                        <div class="col-sm-2">
+                            <span class="text-danger">* <?php echo $fullNameErr;?></span>
                         </div>
                     </div>
                     <div class="mb-3 row">
@@ -45,7 +71,7 @@
                     <div class="mb-3 row">
                         <label for="dob" class="col-sm-2 col-form-label">Date of Birth</label>
                         <div class="col-sm-10">
-                            <input type="date" class="form-control" id="dob" name="dob">
+                            <input type="date" class="form-control" id="dob" name="dob" value="<?php echo $dob;?>">
                         </div>
                     </div>
                     <div class="mb-3 row">
@@ -56,13 +82,16 @@
                     </div>
                     <div class="mb-3 row">
                         <label for="gender" class="col-sm-2 col-form-label">Gender</label>
-                        <div class="col-sm-10">
+                        <div class="col-sm-8">
                             <select class="form-select" id="gender" name="gender" aria-label="Gender Select">
                                 <option value="" <?php if($gender == ''){echo "selected";}?>>Select Gender</option>
                                 <option value="M" <?php if($gender == 'M'){echo "selected";}?>>Male</option>
                                 <option value="F" <?php if($gender == 'F'){echo "selected";}?>>Female</option>
                                 <option value="O" <?php if($gender == 'O'){echo "selected";}?>>Others</option>
-                            </select>
+                            </select>                            
+                        </div>
+                        <div class="col-sm-2">
+                            <span class="text-danger">* <?php echo $genderErr;?></span>
                         </div>
                     </div>
                     <div class="mb-3 row">
---
+++ newApp/students.php
@@ -0,0 +1,60 @@
+<?php
+    require('connection.inc.php');
+    $message = '';
+    if($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['message'])) {
+        $message = test_input($_GET['message']);
+    }
+    $students_sql = "SELECT * FROM `students`";
+    $result = mysqli_query($conn,$students_sql);    
+?>
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>All Students</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <?php if(!empty($message)) {?>
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>Congratulations!</strong> <?php echo $message;?>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+        <?php }?>
+        <h1>Student Registration Form</h1>
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">ID</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Date of Birth</th>
+                    <th scope="col">Phone</th>
+                    <th scope="col">Gender</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php $srno = 1; while($row = mysqli_fetch_assoc($result)) {?>
+                <tr>
+                    <th scope="row"><?php echo $srno;?></th>
+                    <td><?php echo $row['id'];?></td>
+                    <td><?php echo $row['name'];?></td>
+                    <td><?php echo $row['email'];?></td>
+                    <td><?php echo $row['dob'];?></td>
+                    <td><?php echo $row['phone'];?></td>
+                    <td><?php echo $row['gender'];?></td>
+                </tr>
+                <?php $srno++; }?>
+            </tbody>
+        </table>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
